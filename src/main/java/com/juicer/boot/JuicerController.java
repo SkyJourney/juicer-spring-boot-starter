4eb59e4826dfe296925616a47011b74f1f68548e
SkyJourney <sky.jiang.s.z@gmail.com>
2020-01-21 14:45:14 +0800

    First Version

--- src/main/java/com/juicer/boot/JuicerController.java
+++ src/main/java/com/juicer/boot/JuicerController.java
@@ -4,11 +4,13 @@ import com.juicer.JuicerActuator;
 import com.juicer.JuicerHandlerFactory;
 import com.juicer.core.JuicerData;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
@@ -30,15 +32,26 @@ public class JuicerController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/{handler}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{handler}", method = {RequestMethod.GET,RequestMethod.POST})
     public List<JuicerData> getData( @PathVariable String handler) throws ExecutionException, InterruptedException {
         return juicerActuator.getDataFromHandler(handler);
     }
 
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/handlers", method = RequestMethod.POST)
     public Set<String> getHandlers() {
         return juicerHandlerFactory.getHandlerSet();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getHandlersView(final ModelMap model) {
+        List<JuicerHandlerInfo> juicerHandlerInfos = new ArrayList<>();
+        for(String handler: juicerHandlerFactory.getHandlerSet()) {
+            String previous = juicerHandlerFactory.getJuicerHandler(handler).getParent();
+            juicerHandlerInfos.add(new JuicerHandlerInfo(handler, "".equals(previous)?"-":previous));
+        }
+        model.addAttribute("handlers", juicerHandlerInfos);
+        return "juicer/handler";
     }
 
 }
---
+++ src/main/java/com/juicer/boot/JuicerHandlerInfo.java
@@ -0,0 +1,34 @@
+package com.juicer.boot;
+
+/**
+ * @author SkyJourney
+ */
+public class JuicerHandlerInfo {
+
+    private String handler;
+    private String previous;
+
+    public JuicerHandlerInfo(String handler, String previous) {
+        this.handler = handler;
+        this.previous = previous;
+    }
+
+    public JuicerHandlerInfo() {
+    }
+
+    public String getHandler() {
+        return handler;
+    }
+
+    public void setHandler(String handler) {
+        this.handler = handler;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+}
